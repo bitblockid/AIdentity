40beba2a5e2052393173ab3d5a6b10d361197028
bitblockid <189543937+bitblockid@users.noreply.github.com>
2024-11-24 20:13:13 +0100

    Add files via upload

--- scripts/registerModel.js
+++ scripts/registerModel.js
@@ -1,16 +1,16 @@
 const Web3 = require("web3");
 const contract = require("./build/contracts/OpenAIDRegistry.json");
 
-async function registerModel() {
+async function registerModel(modelId, attributes) {
     const web3 = new Web3("http://localhost:8545");
     const networkId = await web3.eth.net.getId();
     const deployedNetwork = contract.networks[networkId];
     const registry = new web3.eth.Contract(contract.abi, deployedNetwork.address);
 
     const accounts = await web3.eth.getAccounts();
-    await registry.methods.registerModel("model1", "attributes").send({ from: accounts[0] });
+    await registry.methods.registerModel(modelId, attributes).send({ from: accounts[0] });
 
-    console.log("Model registered!");
+    console.log(`Model ${modelId} registered successfully!`);
 }
 
-registerModel();
+registerModel("model1", "initial attributes");
